52257321edd259392d2072588462ac86715e5def
priyankamohanty18 <50870196+priyankamohanty18@users.noreply.github.com>
2025-11-05 09:47:25 +0000

    added .env file

---
+++ video_stats.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env")
+
+API_KEY = os.getenv("API_KEY")
+CHANNEL_HANDLE = "MrBeast"
+
+def get_playlistid():
+    try:
+        url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
+
+        response = requests.get(url)
+        response.raise_for_status()
+
+        #print (response)
+
+        data = response.json()
+        #print(json.dumps(data, indent=4))
+
+        #data.items[0].contentDetails.relatedPlaylists.uploads
+
+        channel_items = data['items'][0]
+        channel_playlistid = channel_items['contentDetails']['relatedPlaylists']['uploads']
+        #print(channel_playlistid)
+        return channel_playlistid
+        
+    except requests.exceptions.RequestException as e:
+        raise e
+    
+if __name__ == "__main__":
+    get_playlistid()     
+        
